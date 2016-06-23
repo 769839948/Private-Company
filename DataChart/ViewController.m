8e694242186056f4a03ddfa96a3814b0f32edc38
769839948 <769839948@qq.com>
2016-06-24 00:16:40 +0800

    another test

--- DataChart/ViewController.m
+++ DataChart/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    //测试项目；
     //hello world
     // Do any additional setup after loading the view, typically from a nib.
 }
