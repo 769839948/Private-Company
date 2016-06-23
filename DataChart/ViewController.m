6595fb298158ebbc95f070fad4c60f12eadb1c95
769839948 <769839948@qq.com>
2016-06-24 00:48:37 +0800

    commet

--- DataChart/ViewController.m
+++ DataChart/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"title";
+//    self.tableview Cpntrpper\\\\
     // Do any additional setup after loading the view, typically from a nib.
 }
 
