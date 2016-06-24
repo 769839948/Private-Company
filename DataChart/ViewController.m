7b48127b7b5cfea9711d93e23d1a268343b1b10b
769839948 <769839948@qq.com>
2016-06-24 23:00:41 +0800

    test

--- DataChart/ViewController.m
+++ DataChart/ViewController.m
@@ -17,21 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"title";
-<<<<<<< HEAD
-<<<<<<< HEAD
-    self.view.backgroundColor = [UIColor redColor];
-=======
-//    self.tableview Cpntrpper\\\\
-=======
-    self.view.backgroundColor = [UIColor whiteColor];
-//    s
-    //测试项目；
-    //hello world
->>>>>>> master
-=======
-    self.view.backgroundColor = [UIColor whiteColor];
-    self.view.backgroundColor = [UIColor redColor];
->>>>>>> cf65d293bd3becb430964530162a407500a7e48b
+
     // Do any additional setup after loading the view, typically from a nib.
 }
 
